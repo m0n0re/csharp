0ac1ba20dd80e26589a74f10fba874c7ce31510c
monore <goldpocket01@gmail.com>
2023-01-02 00:22:39 +0900

    標準入力テスト

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace HelloWorld
 {
@@ -6,7 +7,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Program Srart");
+
+            //文字入力を読み取る
+            String str = Console.ReadLine();
+
+            //入力された文字列を表示
+            Console.WriteLine("str = {0}", str);
+
+            Console.WriteLine("Program end.");
         }
     }
 }
