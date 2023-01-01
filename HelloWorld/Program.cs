e7d578da9bd3a8885fe1e129f7034a5620c7e1d9
monore <goldpocket01@gmail.com>
2023-01-02 01:59:27 +0900

    組み込み型の種類

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,10 +10,10 @@ namespace HelloWorld
             Console.WriteLine("Program Srart");
 
             // 文字入力を読み取るにはConsole.ReadLine()を使う
-            String str = Console.ReadLine();
+            // String str = Console.ReadLine();
 
             // 入力された文字列を表示
-            Console.WriteLine("str = {0}", str);
+            Console.WriteLine("str = {0}");
 
             // var：型推論
             // varは型推論なので右辺がないとコンパイルエラーがでる
@@ -33,6 +33,33 @@ namespace HelloWorld
                 string hello = "hello";
                 string bye = "bye!";
             }
+
+            // 組み込み型の種類
+            // 単純型
+            // 整数型
+            byte tByte = 0;     // 8ビット整数 0 ～ 255
+            short tShort = 0;   // 16ビット整数 -32,768 ～ 32,767
+            ushort tUshort = 0; // 符号なし 0 ～ 65,535
+            int tInt = 0;       // 32ビット整数 -2,147,483,648 ～ 2,147,483,647
+            long tLong = 0;     // 64ビット整数 -9,223,372,036,854,775,808 ～ 9,223,372,036,854,775,807
+
+            char tChar = 'a';   // 文字型
+
+            float tFloat = 0.1f;    // 浮動小数点型 単精度
+            double tDouble = 0.1D;  // 浮動小数点型 倍精度
+            decimal tDecimal = 0.01M; // デシマル型
+
+            bool tBool = true;  // 論理型
+
+            string tStr = "string"; //文字列型
+            Object tObj = new Object(); // オブジェクト型
+
+            // エスケープシーケンス
+
+            char tChar2 = '\''; // シングルクォート
+            string tStr2 = "\""; // ダブルクォーテーション
+            string tStr3 = "\\"; // バックスラッシュを文字列として使う
+
 
 
             Console.WriteLine("Program end.");
